3067438bb3929e58d47e0fba203d69a2f5873ebf
pratikshya30 <020bim030@sxc.edu.np>
2024-11-23 22:26:55 +0545

    App.jsx updated

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import OurWork from '../components/OurWork'
 import WoodDetails from '../components/WoodDetails'
+import AnyQuestions from '../components/AnyQuestions'
 
 const Gallery = () => {
   return (
     <div className=''>
       <OurWork padding={20}/>
       <WoodDetails/>
+      <AnyQuestions/>
     </div>
   )
 }
